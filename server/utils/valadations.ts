d47c98b561138b358a0119482f2755176a67ffe8
BAssal <baslm8496@gmail.com>
2025-05-27 18:05:08 +0300

    feat: add authentication schema validation using Zod

---
+++ server/utils/valadations.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+export const authSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    name: z.string().min(1).max(255).default('').optional(),
+});
