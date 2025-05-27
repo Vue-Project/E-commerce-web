0b4ab838f88387f4dda41c304068727317d206ce
BAssal <baslm8496@gmail.com>
2025-05-27 16:35:28 +0300

    feat: add login and register pages with social authentication buttons

---
+++ components/Auth/SocialButtons.vue
@@ -0,0 +1,20 @@
+<template>
+    <Button @click="authenticate" type="button" variant="outline">
+        <div class="flex items-center">
+            <Icon :name="icon" class="mr-2 h-4 w-4"></Icon>
+            {{ label }}
+        </div>
+    </Button>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        label: string;
+        icon: string;
+    }>();
+    const authenticate = () => {
+        window.location.href = `/api/auth/github`;
+    };
+</script>
+
+<style lang="scss" scoped></style>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -4,17 +4,21 @@
             <form action="">
                 <Card>
                     <CardHeader>
-                        <CardTitle class="text-2xl">Login</CardTitle>
-                        <CardDescription>Enter your information below to login your account.</CardDescription>
+                        <CardTitle class="text-2xl">Register</CardTitle>
+                        <CardDescription>Enter your information below to create your account.</CardDescription>
                     </CardHeader>
                     <CardContent class="grid gap-4">
                         <div class="grid gap-2">
+                            <Label for="name">Name</Label>
+                            <Input id="name" placeholder="name" v-model="formRegister.name" />
+                        </div>
+                        <div class="grid gap-2">
                             <Label for="email">Email</Label>
-                            <Input id="email" type="email" placeholder="m@example.com" v-model="formLogin.email" />
+                            <Input id="email" type="email" placeholder="m@example.com" v-model="formRegister.email" />
                         </div>
                         <div class="grid gap-2">
                             <Label for="password">Password</Label>
-                            <Input id="password" type="password" v-model="formLogin.password" />
+                            <Input id="password" type="password" v-model="formRegister.password" />
                         </div>
                         <div class="grid cols-1">
                             <AuthSocialButtons icon="radix-icons:github-logo" label="Github"></AuthSocialButtons>
@@ -23,8 +27,8 @@
                     <CardFooter class="flex-col space-y-2">
                         <Button class="w-full" type="submit">Register</Button>
                         <p>
-                            Don't have an account?
-                            <NuxtLink to="/auth/register" class="border-b border-gray-500 text-muted-foreground hover:text-primary">Register</NuxtLink>
+                            Already have an account?
+                            <NuxtLink to="/auth/login" class="border-b border-gray-500 text-muted-foreground hover:text-primary">Login</NuxtLink>
                         </p>
                     </CardFooter>
                 </Card>
@@ -35,10 +39,12 @@
 
 <script setup lang="ts">
     type PAYLOAD = {
+        name: string;
         email: string;
         password: string;
     };
-    const formLogin = ref<PAYLOAD>({
+    const formRegister = ref<PAYLOAD>({
+        name: '',
         email: '',
         password: '',
     });
