5873d35fc46a19ca5d694dd80618581d6ea294ff
BAssal <baslm8496@gmail.com>
2025-05-27 10:19:20 +0300

    feat: add login and home page components
    - Created a new login page at /auth/login with basic template.
    - Added a home page at / with links to login and register pages.

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Home page</div>
+    <nuxt-link to="/auth/login">login</nuxt-link>
+    <nuxt-link to="/auth/register">resgister</nuxt-link>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
