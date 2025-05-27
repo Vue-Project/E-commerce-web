255d7312bd9f376b6b02e897f01b756a12b53b5b
BAssal <baslm8496@gmail.com>
2025-05-27 16:35:39 +0300

    feat: implement GitHub OAuth handler and user sanitization utility

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>Home page</div>
+    <pre>{{ user }}</pre>
+
     <nuxt-link to="/auth/login">login</nuxt-link>
     <nuxt-link to="/auth/register">resgister</nuxt-link>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    const { user } = useUserSession();
+</script>
 <style scoped></style>
