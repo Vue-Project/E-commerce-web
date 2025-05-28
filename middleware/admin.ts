dddaf99a095e39df8f48e30db6ffd0033d996356
BAssal <baslm8496@gmail.com>
2025-05-29 01:14:21 +0300

    fix: correct admin route navigation logic and improve code readability

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async () => {
     const { user } = useUserSession();
     if (!user.value) return navigateTo('/auth/login');
-    if (user.value && user.value.role !== 'ADMIN')
-
-})
+    if (user.value && user.value.role !== 'ADMIN') {
+        return navigateTo('/auth/login');
+    }
+});
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
 
     <nuxt-link to="/auth/login">login</nuxt-link>
     <nuxt-link to="/auth/register">resgister</nuxt-link>
+    <nuxt-link to="/admin">Admin</nuxt-link>
     <button @click="logout">logout</button>
 </template>
 <script setup lang="ts">
